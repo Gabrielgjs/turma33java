54360c3a8d4bb40b6adb2d567093a9c8ff5a9009
Gabrielgjs <gjsgabriel83@gmail.com>
2021-09-09 14:19:26 -0300

    correcao

--- java/Heranca_Polimorfismo/HerancaEpolimorfismo/src/Application/Principal.java
+++ java/Heranca_Polimorfismo/HerancaEpolimorfismo/src/Application/Principal.java
@@ -10,16 +10,16 @@ public class Principal {
 	
 	public static void main(String[] args) {
 
-	Scanner sc = new Scanner(System.in);
+	Scanner teclado = new Scanner(System.in);
 	
-	Cachorro cachorro1 = new Cachorro("Cão chamado Bob", 7,"latir", "corre");
-	Cavalo cavalo1 = new Cavalo("Cavalo chamado Keny", 8, "relinchar", "trota");
+	Cachorro cachorro1 = new Cachorro("Cão chamado luck", 7,"latir", "pular");
+	Cavalo cavalo1 = new Cavalo("Cavalo chamado java", 8, "relinchar", "coice");
 	Preguica preguica1 = new Preguica("Preguiça chamado Pregui", 9, "hammmm","sobe em árvores");
 	
 	cachorro1.exibir();
 	cavalo1.exibir();
 	preguica1.exibir();
 	
-	sc.close();
+	teclado.close();
 }
 }
