a836dcc542dd17b3184d92aac2aa6a6d2e9be4b9
Gabrielgjs <gjsgabriel83@gmail.com>
2021-09-23 21:35:01 -0300

    hello Spring

---
+++ mySql/lista1/exemploLivrosFemme.sql
@@ -0,0 +1,13 @@
+CREATE database db_femme;
+
+use db_femme;
+
+create table tb_livros(
+id_livros bigint(5) auto_increment,
+nome varchar(40) not null,
+lancamento date,
+primary key(id_livros)
+);
+
+update 
+
