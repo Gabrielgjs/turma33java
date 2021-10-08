277481eb7a18b31ac66e962cf83d2df6e1622c09
Gabrielgjs <gjsgabriel83@gmail.com>
2021-10-08 14:30:12 -0300

    jUnit test

--- spring/blogPessoal/src/main/java/generation/org/blogPessoal/model/Usuario.java
+++ spring/blogPessoal/src/main/java/generation/org/blogPessoal/model/Usuario.java
@@ -9,46 +9,68 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
-@Table(name = "tb_usuario")
+@Table(name ="usuarios")
 public class Usuario {
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
 	@NotNull
-	@Size(min = 2, max = 100)
+	@Size(min = 5, max = 100)
 	private String nome;
+	
 	@NotNull
 	@Size(min = 5, max = 100)
 	private String usuario;
+	
 	@NotNull
-	@Size(min = 5, max = 100)
+	@Size(min = 5)
 	private String senha;
 	
-	//Getters and Setters
-	
+	// Primeiro método Construtor
+
+	public Usuario(long id, String nome, String usuario, String senha) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+	}
+
+	// Segundo método Construtor
+
+	public Usuario() {	}
+
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getUsuario() {
 		return usuario;
 	}
+
 	public void setUsuario(String usuario) {
 		this.usuario = usuario;
 	}
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+	
 }
--- spring/blogPessoal/src/main/java/generation/org/blogPessoal/repository/UsuarioRepository.java
+++ spring/blogPessoal/src/main/java/generation/org/blogPessoal/repository/UsuarioRepository.java
@@ -1,17 +1,20 @@
 package generation.org.blogPessoal.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import generation.org.blogPessoal.model.Usuario;
 
-
-
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 	//vai pegar um usuario e um username
 	public Optional<Usuario> findByUsuario(String usuario); 
 	//findBy é um nome reservado, o Usuario é exatamente o nome do atributo qeu criamos na model Usuario.
+
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String string);
+
+	public Usuario findAllNome(String string);
 
 }
